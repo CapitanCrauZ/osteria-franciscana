54ad48bc32bb6f97359a18e9ea45b80d66396a2e
Paolo Rios <paologhost14.pr@gmail.com>
2022-08-26 16:17:25 -0400

    language from es-cl to en-us

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -114,7 +114,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'es-cl'
+LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
 
